302c9db3842e67c0b7dfee4ba9958211937c95af
Parvathyn95 <parvathycet95@gmail.com>
2022-10-31 08:30:27 +0400

    TestNG Assignment

---
+++ TestNGAssignment/src/main/java/com/obsqura/TestNGAssignment/BaseTestNg.java
@@ -0,0 +1,26 @@
+package com.obsqura.TestNGAssignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTestNg {
+	public WebDriver driver;
+	@BeforeMethod
+	public void initBrowser() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\admin\\eclipse-workspace\\JUnitSample\\src\\main\\java\\Resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		
+	}
+	public void browserClose() {
+		driver.close();
+	}
+	@AfterMethod
+	public void browserQuit() {
+		driver.quit();
+	}
+
+}
---
+++ TestNGAssignment/src/test/java/com/obsqura/TestNGAssignment/TestCasesNg.java
@@ -0,0 +1,34 @@
+package com.obsqura.TestNGAssignment;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCasesNg extends BaseTestNg{
+	@Test
+	public void verifyTitle() {
+		String actualTitle,expTitle="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+		actualTitle = driver.getTitle();
+		Assert.assertEquals(expTitle, actualTitle);
+	}
+	@Test
+	public void verifyUrl() {
+		String actualUrl;
+		actualUrl=driver.getCurrentUrl();
+		Assert.assertNotNull(actualUrl);
+	}
+	@Test
+	public void verifyPageSource() {
+		String actualSource,expSource="https";
+		actualSource=driver.getPageSource();
+		Boolean verifySource=actualSource.contains(expSource);
+		Assert.assertTrue(verifySource);
+	}
+	@Test
+	public void navigateandCheck() {
+		String loginTitle;
+		driver.navigate().to("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
+		loginTitle=driver.getTitle();
+		Boolean verTitle = loginTitle.equalsIgnoreCase("Amazon Sign In");
+		Assert.assertTrue(verTitle);
+	}
+}
