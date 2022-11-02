251de4d7ce7eb918af46ca9025ccdfe8f81c1086
Parvathyn95 <parvathycet95@gmail.com>
2022-11-02 15:37:24 +0400

    Clicking on various menus across all

--- TestNGAssignment/src/test/java/com/obsqura/TestNGAssignment/LocatorsId.java
+++ TestNGAssignment/src/test/java/com/obsqura/TestNGAssignment/LocatorsId.java
@@ -12,5 +12,17 @@ public class LocatorsId extends BaseTestNg  {
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("lakme eyeconic kajal");
 		driver.findElement(By.id("nav-search-submit-button")).click();
 	}
+	@Test(description="locating by class All")
+	public void locatingAll() {
+		driver.findElement(By.className("hm-icon-label")).click();
+		driver.findElement(By.xpath("//a[@href='/gp/bestsellers/?ref_=nav_em_cs_bestsellers_0_1_1_2']")).click();
+	}
+	@Test(description="locating by class Electronics Fashion")
+	public void locatingElectronicsnFashion() {
+		driver.findElement(By.xpath("//a[@href='/electronics/b/?ie=UTF8&node=976419031&ref_=nav_cs_electronics']")).click();
+		driver.findElement(By.xpath("//a[@class='nav-logo-link nav-progressive-attribute']")).click();
+		driver.findElement(By.xpath("//a[@href='/gp/browse.html?node=6648217031&ref_=nav_cs_fashion']")).click();	
+		driver.findElement(By.xpath("//a[@class='nav-logo-link nav-progressive-attribute']")).click();
+	}
 
 }
