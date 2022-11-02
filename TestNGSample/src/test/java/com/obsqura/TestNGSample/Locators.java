41628b0323a301710f73f743bdd3a61a1477f049
Parvathyn95 <parvathycet95@gmail.com>
2022-11-02 15:42:36 +0400

    dropdown and xpath

--- TestNGSample/src/test/java/com/obsqura/TestNGSample/Locators.java
+++ TestNGSample/src/test/java/com/obsqura/TestNGSample/Locators.java
@@ -16,8 +16,23 @@ public class Locators extends Base {
 	public void locatingByClass() {
 		driver.findElement(By.className("top-logo"));
 	}
-	@Test(description="Locating By Class Amazon")
+	@Test(description="Locating By Class Search n Featured")
 	public void locatingClass() {
-		driver.findElement(By.className("hm-icon-label")).click();
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone 14 pro max case");
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		driver.findElement(By.className("a-dropdown-label")).click();
+		driver.findElement(By.id("twotabsearchtextbox")).clear();
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("lakme eyeconic kajal");
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		driver.findElement(By.className("a-dropdown-prompt")).click();	
+	}
+	@Test(description="Locating By Class All")
+	public void locatingAll() {
+		driver.findElement(By.className("hm-icon-label")).click();	
+	}
+	@Test(description="Locating By Class Flag")
+	public void locatingFlag() {
+		driver.findElement(By.xpath("//a[@class='nav-a nav-a-2 icp-link-style-2']")).click();	
+		driver.findElement(By.className("a-button-input")).click();
 	}
 }
